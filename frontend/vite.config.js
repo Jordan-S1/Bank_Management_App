be8a6ac2bcc55739840deae73e5ba774bb1f3e17
Jordan-S1 <jordanshodipo1@gmail.com>
2025-05-24 06:49:36 +0100

    updated file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://bank-management-app-server.onrender.com",
+      "/api": "http://localhost:4000",
     },
   },
 });
